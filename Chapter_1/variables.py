54bb038bdc2d1e1a59aa819e4869de58c1734d42
Nityam <nityamthakur14@gmail.com>
2026-07-02 18:24:37 +0530

    Add Chapter 1 files

---
+++ Chapter_1/variables.py
@@ -0,0 +1,21 @@
+# dynamic programming language as we can values of variables
+# also flexible cuz it doesn't reqire any specification of data type in variable
+number1 = 6
+print(number1)
+number1 =9
+print(number1)
+# string , number 
+name =" harshit"
+print(name)
+name = 123
+print (name)
+# rules for variable implementation
+# 1. can't start with any number
+# 2. u can start with any aplhabet or underscore
+# 3. can't use any special symbol in variable at any palce
+
+# conventions
+user_one_name = "homicide" #snake case writing usually in python
+print(user_one_name)
+UserOneName = "gujju" #camel case writing usually in java
+print(UserOneName)
